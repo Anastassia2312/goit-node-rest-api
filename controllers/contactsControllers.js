b7ab846754ae9e20e45c7ed5919e877bbb4c6f83
Anastassia2312 <23122000na@gmail.com>
2024-05-05 15:11:45 +0300

    Summary

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,12 +1,13 @@
-import contactsService from "../services/contactsServices.js";
 import {
   createContactSchema,
   updateContactSchema,
+  updateStatusSchema,
 } from "../schemas/contactsSchemas.js";
+import Contact from "../model/contactsModel.js";
 
-export const getAllContacts = async (req, res) => {
+export const getAllContacts = async (req, res, next) => {
   try {
-    const contacts = await contactsService.listContacts();
+    const contacts = await Contact.find();
     res.status(200).json(contacts);
   } catch (error) {
     next(error);
@@ -16,8 +17,7 @@ export const getAllContacts = async (req, res) => {
 export const getOneContact = async (req, res) => {
   try {
     const { id } = req.params;
-    console.log(id);
-    const contactById = await contactsService.getContactById(id);
+    const contactById = await Contact.findById(id);
     if (!contactById) {
       return res.status(404).json({ message: "Not found" });
     }
@@ -30,7 +30,7 @@ export const getOneContact = async (req, res) => {
 export const deleteContact = async (req, res) => {
   try {
     const { id } = req.params;
-    const removeContact = await contactsService.removeContact(id);
+    const removeContact = await Contact.findByIdAndDelete(id);
     if (!removeContact) {
       return res.status(404).json({ message: "Not found" });
     }
@@ -46,8 +46,13 @@ export const createContact = async (req, res, next) => {
     if (error) {
       return res.status(400).json({ message: error.message });
     }
-    const { name, email, phone } = req.body;
-    const newContact = await contactsService.addContact(name, email, phone);
+    const book = {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      favorite: req.body.favorite,
+    };
+    const newContact = await Contact.create(book);
     res.status(201).json(newContact);
   } catch (error) {
     next(error);
@@ -66,17 +71,43 @@ export const updateContact = async (req, res) => {
       return res.status(400).json({ message: error.message });
     }
     const { id } = req.params;
-    const { name, email, phone } = req.body;
-    const updateContact = await contactsService.updateContact(
-      id,
-      name,
-      email,
-      phone
-    );
+    const { name, email, phone, favorite } = req.body;
+    const contact = {
+      name: name,
+      email: email,
+      phone: phone,
+      favorite: favorite,
+    };
+    const updateContact = await Contact.findByIdAndUpdate(id, contact, {
+      new: true,
+    });
     if (!updateContact) {
       return res.status(404).json({ message: "Not found" });
     }
     res.status(200).json(updateContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateStatusContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { error } = updateStatusSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+    const book = {
+      favorite: req.body.favorite,
+    };
+    const contact = await Contact.findByIdAndUpdate(id, book, {
+      new: true,
+    });
+
+    if (!contact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(contact);
   } catch (error) {
     next(error);
   }
